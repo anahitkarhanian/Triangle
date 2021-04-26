7ba70fb3c90a41299f1e61c263ca293274c9d406
anahitkarhanian <anahit.karhanyan777@gmail.com>
2021-04-26 17:41:19 +0400

    Triangle

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 3;
+	int b = 4;
+	int c = 5;
+	int area, perimeter;
+	area  = a*b/2;
+	perimeter = a+b+c;
+	System.out.println(area);
+	System.out.println(perimeter);
+
+
+
+    }
+}
